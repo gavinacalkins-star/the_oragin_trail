2b289580ba09c4128c23c8b4fb23f55b6e6c2af5
gavinacalkins-star <246409284+gavinacalkins-star@users.noreply.github.com>
2026-07-21 18:01:32 -0700

    Remove unused child variables and related save functionality

--- main.py
+++ main.py
@@ -23,10 +23,6 @@ day = 0
 save_file = "saves/save.json"
 saves_list = [f for f in os.listdir("saves") if os.path.isfile(os.path.join("saves", f))]
 goto_next_day = True
-children = 0
-child1_name = None
-child2_name = None
-child3_name = None
 
 
 def save_variable(var_name, value):
@@ -73,11 +69,6 @@ def info():
     print(f"wagon name: {wagon_name}")
     time.sleep(0.05)
     print(f"pioner name: {pioner_name}")
-    time.sleep(0.05)
-    print(f"Children: {children}")
-    print(f"    Child 1: {child1_name}")
-    print(f"    Child 2: {child2_name}")
-    print(f"    Child 3: {child3_name}")
     time.sleep(0.05)
     if wagon_damage > 0:
         print("you will travel slower because your wagon is damaged")
@@ -182,10 +173,6 @@ if input() == "1":
     wagon_name = get_saved_value("wagon_name")
     pioner_name = get_saved_value("pioner_name")
     alive = get_saved_value("alive")
-    children = get_saved_value("children")
-    child1_name = get_saved_value("child1_name")
-    child2_name = get_saved_value("child2_name")
-    child3_name = get_saved_value("child3_name")
     print("Resuming saved game...")
     time.sleep(1)  # Simulate the passage of time
     print(".")
@@ -308,14 +295,6 @@ while alive and distance_traveled < distance_needed:
             time.sleep(0.05)
             (save_variable("alive", alive))
             print("saved alive")
-            (save_variable("children", children))
-            print("saved children")
-            (save_variable("child1_name", child1_name))
-            print("saved child1_name")
-            (save_variable("child2_name", child2_name))
-            print("saved child2_name")
-            (save_variable("child3_name", child3_name))
-            print("saved child3_name")
             time.sleep(0.05)
             os.rename("saves/save.json", f"saves/{input('Enter the name for your savegame: ')}.json")
             print("saved save")
